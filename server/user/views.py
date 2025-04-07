31a63f38443541532045fe2dc64f3a0dfd28e5d0
Khant Thit Oo <127503879+travthegoat@users.noreply.github.com>
2025-04-07 20:03:43 +0630

    Added a get with username feature and current_field in user model

--- server/user/views.py
+++ server/user/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from djoser.views import UserViewSet
 
-# Create your views here.
+from .models import User
+from .serializers import UserSerializer
+
+class CustomUserViewSet(UserViewSet):
+    @action(detail=False, methods=['GET'], url_path='username/(?P<username>[^/.]+)')
+    def get_by_username(self, request, username=None):
+        user = get_object_or_404(self.get_queryset(), username=username)
+        serializer = self.get_serializer(user)
+        return Response(serializer.data)
