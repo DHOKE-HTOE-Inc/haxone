5d64562d25dd265e289138b1e875385bc5af90a7
Htet Aung Lwin <htetaunglwin.canon@gmail.com>
2025-04-09 10:56:33 +0630

    all done user profile setting

--- server/user/serializers.py
+++ server/user/serializers.py
@@ -10,7 +10,8 @@ class UserCreateSerializer(BaseUserCreateSerializer):
 class UserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
         model = User
-        fields = ('id', 'username', 'email', 'display_name', 'profile_img', 'skills', 'location', 'current_job', 'is_staff', 'joined_at')
+        fields = ('id', 'username', 'email', 'display_name', 'profile_img', 'skills', 'location', 'current_job', 'aboutme', 'is_staff', 'joined_at')
+        read_only_fields = ['username', 'email']
         
 class SimpelUserSerializer(serializers.ModelSerializer):
     class Meta:
